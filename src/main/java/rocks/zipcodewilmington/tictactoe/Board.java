6e863cc78800e7c6dd7c65c8c1bd084075cd40b6
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-05-30 13:31:17 -0400

    completed:

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,180 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character [][] state;
     public Board(Character[][] matrix) {
+        state = matrix;
     }
 
+
+
     public Boolean isInFavorOfX() {
-        return null;
+        boolean win = false;
+        int rowCounter = 0;
+        int colCounter = 0;
+        int diagCounter = 0;
+        int forDiagCounter = 0;
+
+        //check row for win
+        for (int row = 0; row < state.length; row++){
+
+            for (int col = 0; col < state[row].length; col++){
+                if(rowCounter == 3){
+                    break;
+                }
+
+                if (state[row][col] == 'X') {
+                    rowCounter++;
+
+                }
+                else {
+                    rowCounter = 0;
+                    break;
+                }
+
+            }
+        }
+
+
+
+
+        //check column for win
+        for (int col = 0; col < state[0].length; col++) {
+            for (int row = 0; row < state.length; row++) {
+                if(colCounter == 3){
+                    break;
+                }
+                if (state[row][col] == 'X') {
+                    colCounter++;
+                }
+                else {
+                    colCounter = 0;
+                    break;
+                }
+
+            }
+        }
+
+
+
+
+        //check diag /
+        for(int i = 0; i <state.length; i++){
+            if (state[i][i] != 'X'){
+                break;
+            }
+            else {diagCounter++;}
+        }
+
+
+        //check diag \
+        for (int i = 0; i< state.length; i++){
+            if (state[i][state.length-i-1]!= 'X'){
+                break;
+            }
+            else {forDiagCounter++;}
+        }
+
+
+
+        if (rowCounter == 3 || colCounter == 3 || diagCounter == 3 || forDiagCounter == 3){
+            win = true;
+        }
+
+        return win;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        boolean win = false;
+        int rowCounter = 0;
+        int colCounter = 0;
+        int diagCounter = 0;
+        int forDiagCounter = 0;
+
+        //check row for win
+        for (int row = 0; row < state[0].length; row++){
+            for (int col = 0; col < state[0].length; col++){
+                if(rowCounter == 3){
+                    break;
+                }
+
+                if (state[row][col] == 'O') {
+                    rowCounter++;
+
+                }
+                else {
+                    rowCounter = 0;
+                    break;
+                }
+            }
+        }
+
+
+        //check column for win
+        for (int col = 0; col < state[0].length; col++) {
+            for (int row = 0; row < state.length; row++) {
+                if(colCounter == 3){
+                    break;
+                }
+                if (state[row][col] == 'O') {
+                    colCounter++;
+                }
+                else {
+                    colCounter = 0;
+                    break;
+                }
+            }
+        }
+
+
+
+        //check diag /
+        for(int i = 0; i <state.length; i++){
+            if (state[i][i] != 'O'){
+                break;
+            }
+            else {diagCounter++;}
+        }
+
+
+
+        //check diag \
+        for (int i = 0; i< state.length; i++){
+            if (state[i][state.length-i-1]!= 'O'){
+                break;
+            }
+            else {forDiagCounter++;}
+        }
+
+
+
+        if (rowCounter == 3 || colCounter == 3 || diagCounter==3 || forDiagCounter == 3){
+            win = true;
+        }
+
+        return win;
     }
 
     public Boolean isTie() {
-        return null;
+        boolean tie = false;
+        if (!isInFavorOfO() && !isInFavorOfX()) {
+            tie = true;
+            }
+        return tie;
     }
 
     public String getWinner() {
-        return null;
+        String winner = "";
+        if (isInFavorOfO()) {
+            winner = "O";
+        }
+        else if (isInFavorOfX()) {
+            winner = "X";
+        }
+        else {winner = "";}
+
+
+        return winner;
     }
 
 }
